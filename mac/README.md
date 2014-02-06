bd7b9165dbed04b400a86af72ef6966cf39ca9be
Sam Halperin <sam@samhalperin.com>
2014-02-05 19:38:57 -0500

    initial commit (moving repo)

---
+++ mac/README.md
@@ -0,0 +1,17 @@
+Windows Hello-World For The Concept 2 Rowing Ergometer API
+
+by Sam Halperin
+
+This is an XCode project that implements
+a sort of simple hello-world for communicating with Concept 2 PM4's 
+over USB. I wrote this because I found the C2 API sample code a little too involved
+for learning the basics of the API, and the suggestions for simple API usage were buried
+in the API documentation.  It should print power output to the screen. 
+
+IDEAS
++  Make it look for PM3's as well
++  Call the horizontal distance routine and calculate a current velocity.
+	
+SEE ALSO
++  [Concept 2 SDK](http://www.concept2.com/service/software/software-development-kit)
++  [Concept 2 Forum for SDK Questions](http://www.c2forum.com/viewforum.php?f=15)
---
+++ win/Erg1/Erg1/Erg1.cpp
@@ -0,0 +1,119 @@
+#include "stdafx.h"
+#include <iostream>
+#include "PM3DDICP.h"
+#include "PM3CsafeCP.h"
+#include <Windows.h>
+
+using namespace std;
+
+/* Many of the tkcmdset... functions will set
+   a numeric error code.  This function will
+   convert that to a human readable value.
+ */
+void printErrorCodeInfo(ERRCODE_T ecode) {
+	char name[40];
+	char text[200];
+	char memo[250];
+
+	tkcmdsetDDI_get_error_name(ecode,name,sizeof(name));
+	tkcmdsetDDI_get_error_text(ecode,text,sizeof(text));
+	
+	cout << "error: " << name << ": " << text << endl;
+}
+
+/* Sanity test:  If all of our dependencies are in the
+right place, then this will print a numeric DLL version. */
+void dllVersion() {
+	UINT16_T version;
+	version =  tkcmdsetDDI_get_dll_version();
+	cout << "dll version: " << version << endl;
+}
+
+/* Currently discovers PM4's.  Prints the number connected.
+TODO: discover PM3's... (see PMSDKDemoDlg.cpp in the C2 demo project for reference)
+*/
+bool ergsConnected() {
+	ERRCODE_T ecode = 0;
+	UINT16_T numCommunicating = 0;
+	UINT16_T numOldPM3Devices = 0;
+	UINT16_T numNewPM3Devices = 0;
+	UINT16_T numPM4Devices = 0;
+
+	ecode = tkcmdsetDDI_discover_pm3s(TKCMDSET_PM4_PRODUCT_NAME, numCommunicating, &numCommunicating);
+	
+	if (ecode) {
+		cout << "ecode (discovery): " << ecode << endl;
+		printErrorCodeInfo(ecode);
+	} else if (numCommunicating) {
+		numPM4Devices = numCommunicating;
+		cout << "num PM4's: " << numPM4Devices << endl;
+		return true;
+	} else {
+		cout << "No PM4's found." << endl;
+	}
+
+	return false;
+}
+
+/* Another sanity test.  Can we talk to the erg and get its
+serial number? */
+void serialNumber(UINT16_T port) {
+	char rspBuffer[32];
+	UINT8_T serialLength = 16; 
+	ERRCODE_T ecode = 0;
+
+	ecode = tkcmdsetDDI_serial_number(port, rspBuffer, serialLength);
+	if (ecode) {
+		cout << "ecode (serial number): " << ecode << endl;
+		printErrorCodeInfo(ecode);
+	} else {
+		cout << "Serial number: " << rspBuffer << endl;
+	}
+}
+
+
+// After CSAFE has been intialized (see main below)
+// get the per-stroke power output.
+// also see http://www.c2forum.com/viewtopic.php?f=15&t=4667
+void printPower(UINT16_T port) 
+{
+	UINT16_T rsp_size = 100, cmd_size = 1;
+	UINT32_T rsp_data[100], cmd_data[1];
+	cmd_data[0] = 0xB4;	
+	ERRCODE_T ecode = 0;
+	int POWER_LSB = 2;  // The position of the least significant byte of the watt data.
+	int POWER_MSB = 3;  // and the most significant byte.
+
+	ecode = tkcmdsetCSAFE_command(port, cmd_size, cmd_data, &rsp_size, rsp_data);
+
+	if (ecode) {
+		printErrorCodeInfo(ecode);
+	} else {
+		// I believe the correct formula here is that power = (MSB * 256) + LSB.
+		// So MSB is usually going to be 0, but might be 1 if Xeno Muller is pulling a 500.
+		UINT32_T watts = (rsp_data[POWER_MSB] * 256) +   rsp_data[POWER_LSB];
+		cout << "Power = " << watts << endl;
+	}
+}
+
+// Connect to the erg, and read power every 500msec.
+// Note: This is per-stroke data, and doesn't always seem to reset to 0 on successive
+// runs of the program!
+int _tmain(int argc, _TCHAR* argv[])
+{
+	UINT16_T port = 0;
+	tkcmdsetDDI_init();
+	dllVersion();
+	
+	if (ergsConnected()) {
+		tkcmdsetCSAFE_init_protocol(1000);
+		serialNumber(port);
+		for (int i = 0; i< 2400; i++) {
+			printPower(port);
+			Sleep(500);  // the data is once per stroke, so faster is not going to give much beter rez.
+		}
+	}
+	cout << "done" << endl;
+	getchar();
+	return 0;
+}
